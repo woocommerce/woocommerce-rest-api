2a479e658f42a79a37d61c832dc2aced1f8ef742
vedanshu.jain <vedanshu.jain.2012@gmail.com>
2020-03-27 01:26:19 +0530

    Add fetch helper method to easily fetch fields from response class.
    The idea is that, response classes will define seperate getters for
    fields that are computationally expensive to calculate. Then these
    getters will be executed only when those fields are requested in the
    response.

--- src/Controllers/Version4/Responses/AbstractObjectResponse.php
+++ src/Controllers/Version4/Responses/AbstractObjectResponse.php
@@ -7,6 +7,8 @@
 
 namespace Automattic\WooCommerce\RestApi\Controllers\Version4\Responses;
 
+use phpDocumentor\Reflection\Types\Object_;
+
 defined( 'ABSPATH' ) || exit;
 
 /**
@@ -22,4 +24,23 @@ abstract class AbstractObjectResponse {
 	 * @return array
 	 */
 	abstract public function prepare_response( $object, $context );
+
+	/**
+	 * Get fields for an object if getter is defined.
+	 *
+	 * @param object $object  Object we are fetching response for.
+	 * @param string $context Context of the request. Can be `view` or `edit`.
+	 * @param array  $fields  List of fields to fetch.
+
+	 * @return array Data fetched from getters.
+	 */
+	public function fetch_fields_using_getters( $object, $context, $fields ) {
+		$data = array();
+		foreach ( $fields as $field ) {
+			if ( method_exists( $this, "get_$field" ) ) {
+				$data[ $field ] = $this->{"get_$field"}( $object, $context );
+			}
+		}
+		return $data;
+	}
 }
